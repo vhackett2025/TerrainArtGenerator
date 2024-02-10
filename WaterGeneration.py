73d95e83c13e828d55428f922257a83bf064274f
vivithequeen <159643239+vivithequeen@users.noreply.github.com>
2024-02-10 21:28:40 +0000

    added tileset

--- WaterGeneration.py
+++ WaterGeneration.py
@@ -1,4 +1,10 @@
-
+import random
+import numpy as np
+import matplotlib.pyplot as plt
 
 def generateWater(waterVal):
-    
+    x=np.linespace(0,10,100)
+    y = np.sin(x)
+    fig, waterGraph = plt.subplots()
+    waterGraph.plot(x,y,color="black")
+    plt.show()
