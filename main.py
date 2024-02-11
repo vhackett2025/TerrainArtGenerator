c3c07c4528767c810c16815849d12ea86c96b37d
Violet <vhackett2025@student.amsacs.org>
2024-02-11 10:56:46 -0500

    .

--- main.py
+++ main.py
@@ -39,14 +39,15 @@ def update_canvas_widget(canvas: tk.Canvas, parameter_maps: dict, climate_variab
                 
             # check for water
             if parameter_maps['height'][x][y] < (int(climate_variables['sea_level'].get())/222):
-                texture_filepath = "textures/waterTileSet/1.png"
+                texture_filepath = "textures/waterTileSet/0.png"
                 if parameter_maps['temperature'][x][y] < 0.25:
                     texture_filepath = "textures/groundTileSet/1_0.png"
             else:
-                texture_filepath = tileset + get_file_name_from_noise_values(parameter_maps['humidity'][x][y], parameter_maps['temperature'][x][y])
+                texture_filepath = tileset + get_file_name_from_noise_values(parameter_maps['humidity'][x][y], parameter_maps['temperature'][x][y] * 1.2)
             img = Image.open(texture_filepath)
             img_enhancer = ImageEnhance.Brightness(img)
-            img = img_enhancer.enhance((parameter_maps['height'][x][y] + 0.5))
+            if "water" not in texture_filepath:
+                img = img_enhancer.enhance((parameter_maps['height'][x][y] + 0.5))
             img = img.resize((16,16))
             if "ground" in texture_filepath:
                 img = img.rotate(90 * random.randrange(4), expand=1)
