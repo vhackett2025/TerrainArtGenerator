1a2ded2691e7628cbef53538fab903e46175505d
vivithequeen <159643239+vivithequeen@users.noreply.github.com>
2024-02-18 13:40:00 +0000

    i forgot to copy the code lol

--- heightGeneration.py
+++ heightGeneration.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 import random 
 from perlin_noise import PerlinNoise
+import math
+from PIL import Image
 
-def generateWater(octaveVal,offsetVal,showPlot=False):
-    size=256
-    
+import math
+import os
+def generateHeight(octaveVal, offsetVal,showPlot = False):
+    size=512
+    ROOT = os.path.dirname(os.path.abspath(__file__))
+
     #size is the square dimentions
     #octaves = thing we change!
     #seed is randomness its just random :P
@@ -15,9 +20,10 @@ def generateWater(octaveVal,offsetVal,showPlot=False):
     for i in range(size):
         row = []
         for j in range(size):
+            
             val=noise([i/size, j/size])
         
-            row.append((val+0.5)+offsetVal/100)
+            row.append(((val+0.5)) + (offsetVal - 50)/100)
             
         pic.append(row)
     
@@ -25,7 +31,25 @@ def generateWater(octaveVal,offsetVal,showPlot=False):
         plt.imshow(pic, cmap='gray')
         plt.grid(False);plt.axis('off')
         plt.show()
-    return pic 
-    #plt.imshow(pic, cmap='gray')
-    #plt.grid(False);plt.axis('off')
-    #plt.show()
+        
+    #os.remove(ROOT+"/tempHeight.png")
+    
+    plt.imshow(pic, cmap='Grays')
+    plt.grid(False);plt.axis('off')
+    #plt.show() 
+    plt.savefig(ROOT+"/tempHeight.png",dpi=300)
+    
+    img=Image.open(ROOT+"/tempHeight.png")
+    if math.floor((255/(100/offsetVal)))-50<0:
+        alphaVal=20
+    else:
+        alphaVal=math.floor((255/(100/offsetVal)))-50
+    
+    img.putalpha(alphaVal)
+    
+    #os.remove(ROOT+"/tempHeightNew.png")
+    img.save(ROOT+"/tempHeightNew.png")
+    #img.show()
+    
+    
+#generateHeight(10, 100)
